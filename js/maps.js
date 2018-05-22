9f27d5ca3a07e2cc490d18140609de9b1f24ea06
namp8 <namp8@hotmail.com>
2018-05-22 10:58:54 +0000

    destinations

---
+++ js/maps.js
@@ -0,0 +1,24 @@
+(function($) {
+    
+    
+    //Google Map
+    var get_latitude = $('#google-map').data('latitude');
+    var get_longitude = $('#google-map').data('longitude');
+
+    function initialize_google_map() {
+        var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
+        var mapOptions = {
+            zoom: 18,
+            scrollwheel: false,
+            center: myLatlng
+        };
+        var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+              animation: google.maps.Animation.DROP
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize_google_map);
+
+})(jQuery);
